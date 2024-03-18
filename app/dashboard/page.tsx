64e1b54aa6468c273d821798de9efc7fb174b533
maishaSupritee <mschowdh@sfu.ca>
2024-03-17 18:39:57 -0700

    Add sign in with google and sign out from dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,32 @@
-import { auth } from "auth";
+import { Button } from "@/components/ui/button";
+import { auth, signOut } from "auth";
 
 export default async function Dashboard() {
   const session = await auth();
+  console.log(session); //keep it to see if we are getting user data correctly or not
 
-  if (session && session.user) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="text-4xl font-semibold">Welcome to Grad Gains {session.user.name}</h1>
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-10">
+      <h1 className="text-4xl font-semibold">Welcome to Grad Gains</h1>
+      <div>
+        {session && session.user ? (
+          <div className="flex h-screen flex-col items-center justify-center gap-5">
+            <h1 className="text-4xl font-semibold">{session.user.name}</h1>
+            <form
+              action={async () => {
+                "use server";
+                await signOut({ redirectTo: "/sign-in" });
+              }}
+            >
+              <Button type="submit" variant={"destructive"}>
+                Sign Out
+              </Button>
+            </form>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
-    );
-  } else {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="text-6xl font-semibold">Grad Gains Dashboard</h1>
-      </div>
-    );
-  }
+    </div>
+  );
 }
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,25 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Provider } from "@/lib/types";
 import discordLogo from "@/public/images/discord-logo.svg";
+import googleLogo from "@/public/images/google-logo.svg";
 import { signIn } from "auth";
 
 export default function SigninPage() {
-  async function handleSignin(formData: FormData) {
-    "use server";
-    const provider = formData.get("provider") as Provider;
-    await signIn(provider);
-  }
-
   return (
     <form
       className="flex h-screen flex-col items-center justify-center gap-10"
-      action={handleSignin}
+      action={async (formData: FormData) => {
+        "use server";
+        const provider = formData.get("provider") as Provider;
+        console.log(provider); //keep for testing
+        await signIn(provider, { redirectTo: "/dashboard" });
+      }}
     >
       <h1 className="text-6xl font-semibold">Grad Gains</h1>
-
-      <Button type="submit" name="provider" value="discord" icon={discordLogo} size="lg">
-        Sign in with Discord
-      </Button>
+      <div className="flex flex-col items-center gap-5">
+        <Button type="submit" name="provider" value="discord" icon={discordLogo} size="lg">
+          Sign in with Discord
+        </Button>
+        <Button type="submit" name="provider" value="google" icon={googleLogo} size="lg">
+          Sign in with Google
+        </Button>
+      </div>
     </form>
   );
 }

9adb35ff4252005e371b451566c84cb9f51dab53
maishaSupritee <mschowdh@sfu.ca>
2024-03-31 23:12:55 -0700

    Setup neon db with authjs and drizzle adapter

--- auth.ts
+++ auth.ts
@@ -1,8 +1,11 @@
+import { db } from "@/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import Discord from "next-auth/providers/discord";
 import Google from "next-auth/providers/google";
 
 const config = {
+  adapter: DrizzleAdapter(db),
   providers: [Discord, Google],
 } satisfies NextAuthConfig;
 

3b97ecbf024062fbe0a39613cafdd9b3c0052769
maishaSupritee <mschowdh@sfu.ca>
2024-04-01 19:08:51 -0700

    Remove env.ts and migrations folder, add dotenv config

--- db/env.ts
+++
@@ -1,5 +0,0 @@
-const envConfig = {
-  DATABASE_URL: process.env.DATABASE_URL!,
-};
-
-export default envConfig;
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
-import envConfig from "@/db/env";
-import "dotenv/config";
-import { Config } from "drizzle-kit";
+import dotenv from "dotenv";
+import type { Config } from "drizzle-kit";
+
+dotenv.config({ path: ".env.local" });
 
 export default {
   schema: "./db/schema.ts",
   driver: "pg",
-  out: "./drizzle",
-  dbCredentials: {
-    connectionString: envConfig.DATABASE_URL,
-  },
+  dbCredentials: { connectionString: process.env.DATABASE_URL! },
 } satisfies Config;

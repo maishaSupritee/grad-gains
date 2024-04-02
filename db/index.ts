31a8569388bf0436c403f365edcb1580cc2c8b9b
maishaSupritee <mschowdh@sfu.ca>
2024-04-01 19:08:51 -0700

    Remove env.ts and migrations folder, add dotenv config

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,6 @@
-import envConfig from "@/db/env";
 import * as schema from "@/db/schema";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(envConfig.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });

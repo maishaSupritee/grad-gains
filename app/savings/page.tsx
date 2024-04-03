83ac37954506ab0fb642d34266b333bb7f128b19
maishaSupritee <mschowdh@sfu.ca>
2024-04-03 12:40:24 -0700

    Create new savings in database dynamically using server form

--- app/savings/page.tsx
+++ app/savings/page.tsx
@@ -1,9 +1,11 @@
 import { auth } from "@/auth";
 import SavingsCard from "@/components/ui/SavingsCard";
 import { Button } from "@/components/ui/button";
-import { type Savings } from "@/db/schema";
+import { db } from "@/db";
+import { savings, type Savings } from "@/db/schema";
 import { CalculateTotalSavings, GetSavingsData } from "@/lib/savingsQueries";
 import { TransactionDetail } from "@/lib/types";
+import { revalidatePath } from "next/cache";
 
 export default async function SavingsPage() {
   const session = await auth();
@@ -16,13 +18,33 @@ export default async function SavingsPage() {
     savingsDataWithTransactions = await GetSavingsData(user.id);
     totalSavings = await CalculateTotalSavings(user.id);
   }
+  const addSavings = async (formData: FormData) => {
+    "use server";
+    const savingsType = formData.get("savingsType") as string;
+    if (user && user.id && savingsType) {
+      await db.insert(savings).values({
+        userId: user.id,
+        type: savingsType,
+      });
+      revalidatePath("/savings");
+    }
+  };
 
   return (
     <div className="flex flex-col gap-5 px-8 pt-20">
-      <form className="flex flex-row items-center justify-between">
+      <form action={addSavings} className="flex flex-row items-center gap-5 pt-3">
+        <input
+          type="text"
+          name="savingsType"
+          placeholder="What are you saving for?"
+          required
+          className="rounded px-2 py-2"
+        />
         <Button type="submit">Add New</Button>
-        <div className="justify-end">Total Amount: ${totalSavings.toFixed(2)}</div>
       </form>
+      <div className="text-2xl font-semibold">
+        Total Savings: <span className="text-primary">${totalSavings.toFixed(2)}</span>
+      </div>
       {savingsDataWithTransactions.map((savingsDataWithTransaction, index) => (
         <SavingsCard
           key={index}

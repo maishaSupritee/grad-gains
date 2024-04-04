ec4fdd6f686943e045e925b3b7f9420b9850181b
maishaSupritee <mschowdh@sfu.ca>
2024-04-04 00:32:31 -0700

    Add grad gains logo to landing page and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 
-import Link from "next/link";
-import { motion } from "framer-motion";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Button } from "@/components/ui/moving-border";
+import appLogo from "@/public/images/gradgains-logo.svg";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 
 export function BackgroundWithButtons() {
   return (
@@ -16,19 +18,22 @@ export function BackgroundWithButtons() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
+        className="relative flex flex-col items-center justify-center gap-4 px-4"
       >
-        <div className="text-6xl md:text-7xl font-bold dark:text-white text-center">
+        <Image src={appLogo} alt="Logo" width={200} height={200}></Image>
+        <div className="text-center text-6xl font-bold dark:text-white md:text-7xl">
           Welcome To GradGains
         </div>
-        <div className="font-light text-2xl md:text-4xl dark:text-neutral-200 py-4 text-center">
+        <div className="py-4 text-center text-2xl font-light dark:text-neutral-200 md:text-4xl">
           Save, Invest and Grow with your University
         </div>
         <Button
           borderRadius="1.75rem"
-          className="bg-green-500 text-black hover:text-white border-neutral-200 dark:border-slate-800 font-semibold"
+          className="border-neutral-200 bg-green-500 font-semibold text-black hover:text-white dark:border-slate-800"
         >
-          <span><Link href={"/sign-in"}>Sign In</Link></span>
+          <span>
+            <Link href={"/sign-in"}>Sign In</Link>
+          </span>
         </Button>
       </motion.div>
     </AuroraBackground>
